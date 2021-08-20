f1990f03d80ecdf87ed5be20246514aa534f8700
Valentin <bagadin@inbox.ru>
2021-08-20 16:57:01 +0500

    Reorganize app structure

--- src/init.jsx
+++ src/init.jsx
@@ -2,14 +2,42 @@ import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import { Provider } from 'react-redux';
 import rootReducer from './reducer';
-import initApi from './api.js';
+import { updateTicker } from './slices/tickers';
 import App from './App.jsx';
+
+const API_KEY = '2d4f83e4dec4abd61adc8138b775853f144146a20dc4b330465d251fa4c6c517';
 
 export default () => {
   const store = configureStore({
     reducer: rootReducer,
   });
-  initApi();
+
+  const socket = new WebSocket(
+    `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`,
+  );
+
+  const sendMessage = (message) => {
+    const stringifiedMessage = JSON.stringify(message);
+
+    socket.send(stringifiedMessage);
+  };
+
+  socket.onopen = () => {
+    sendMessage(
+      {
+        action: 'SubAdd',
+        subs: ['5~CCCAGG~BTC~USD'],
+      },
+    );
+  };
+
+  socket.onmessage = ({ data }) => {
+    const { FROMSYMBOL: symbol, PRICE: price } = JSON.parse(data);
+    if (price === undefined) {
+      return;
+    }
+    store.dispatch(updateTicker({ symbol, price }));
+  };
 
   return (
     <React.StrictMode>
